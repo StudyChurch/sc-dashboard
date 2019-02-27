cd7101217d7c965f6e1728bb6aaba42c09f2a6af
Tanner Moushey <tanner@iwitnessdesign.com>
2019-02-27 15:37:25 -0800

    New static index files

---
+++ app/static/pilgrimage/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+	<!--     Fonts and icons     -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+	<?php wp_head(); ?>
+</head>
+<body class="sidebar-mini">
+<div id="app"></div>
+<!-- built files will be auto injected -->
+<?php wp_footer(); ?>
+</body>
+</html>
