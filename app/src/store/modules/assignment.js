85d1c621edee8cdbb5f8611214cb819feedac66e
Nick Young <nickyoungweb@gmail.com>
2019-05-07 23:21:25 -0600

    Start setting up assignment state management

--- app/src/store/modules/assignment.js
+++ app/src/store/modules/assignment.js
@@ -2,37 +2,25 @@ import AssignmentService from '@/services/AssignmentService.js';
 
 export const namespaced = true;
 
+export const state = {
+    assignments: [],
+    assignmentsTotal: 0,
+    assignment: {
+
+    }
+};
+
 export const mutations = {
-    /*ADD_STUDY(state, study) {
-        state.studies.unshift(study);
-    },
-    SET_STUDIES(state, studies) {
-        state.studies = studies;
-    },
-    SET_STUDIES_TOTAL(state, studiesTotal) {
-        state.studiesTotal = studiesTotal;
-    },
-    SET_STUDY(state, study) {
-        state.study = study;
-    },
-    SET_CHAPTER(state, chapter) {
-        state.chapter = chapter;
-    },
-    SET_NAVIGATION(state, navigation) {
-        state.study.navigation = navigation;
-    }*/
 
     ADD_ASSIGNMENT( state, assignment ) {
-      // TODO
-    },
-    SET_ASSIGNMENT( state, assignment ) {
-        state.assignment = assignment;
+      state.assignments.unshift( assignment );
     }
 };
 
 export const actions = {
     createAssignment( {commit, dispatch }, data ) {
         return AssignmentService.createAssignment( data ).then( response => {
+            commit( 'ADD_ASSIGNMENT', response.data );
             return response.data;
         } );
     },
@@ -55,5 +43,23 @@ export const actions = {
         return AssignmentService.deleteAssignment( itemId ).then( response => {
             return response.data;
         } );
+    },
+
+    fetchAssignments( {commit, dispatch, rootState }, data ) {
+        // TODO
+    },
+
+    fetchAssignment( { commit, getters, state }, { id, params = {} } ) {
+        if ( id === state.assignment.id ) {
+            return state.assignment;
+        }
+
+        // TODO
+    }
+};
+
+export const getters = {
+    getAssignmentById      : state => id => {
+        return state.assignments.find( study => assignment.id === id );
     }
 };
